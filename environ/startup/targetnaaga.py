4800b2ab7f4f711d7ef74777c1748bc2f12fd9cb
tav <tav@espians.com>
2009-06-06 01:33:43 +0100

    Stub files for the naaga interpreter.

---
+++ environ/startup/targetnaaga.py
@@ -0,0 +1,8 @@
+# This file has been placed into the Public Domain by:
+#
+#   tav <tav@espians.com>
+#
+# See documentation/license.txt for more info.
+
+"""A standalone target for the Naaga interpreter."""
+
---
+++ source/naaga/__init__.py
@@ -0,0 +1,7 @@
+# This file has been placed into the Public Domain by:
+#
+#   tav <tav@espians.com>
+#
+# See documentation/license.txt for more info.
+
+"""Naaga interpreter."""
