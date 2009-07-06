6ca0a09cdaf5c767d2abc13ff6f0057a5c37a0c6
fijal <fijal@zygfryd.(none)>
2009-07-06 10:05:14 -0600

    set/get

--- source/client/webkit_bridge/jsobj.py
+++ source/client/webkit_bridge/jsobj.py
@@ -8,23 +8,41 @@ def wrap_js_object(space, js_ctx, js_obj):
     tp = JSValueGetType(js_ctx, js_obj)
     if tp == kJSTypeUndefined:
         return space.wrap(None)
+    elif tp == kJSTypeNumber:
+        return space.wrap(JSValueToNumber(js_ctx, js_obj))
     else:
         raise NotImplementedError(tp)
+
+def unwrap_to_js_object(space, js_ctx, w_obj):
+    if space.is_true(space.isinstance(w_obj, space.w_int)):
+        return JSValueMakeNumber(js_ctx, space.int_w(w_obj))
+    else:
+        raise NotImplementedError()
 
 class JSObject(Wrappable):
     def __init__(self, js_ctx, js_val):
         self.js_ctx = js_ctx
         self.js_val = js_val
 
-    def descr_getattribute(self, space, name):
-        js_name = JSStringCreateWithUTF8CString(name)
+    def descr_get(self, space, w_name):
+        js_name = JSStringCreateWithUTF8CString(space.str_w(w_name))
         return wrap_js_object(space, self.js_ctx,
                               JSObjectGetProperty(self.js_ctx, self.js_val,
                                                   js_name))
-                           
+
+    def descr_set(self, space, w_name, w_value):
+        js_val = unwrap_to_js_object(space, self.js_ctx, w_value)
+        js_name = JSStringCreateWithUTF8CString(space.str_w(w_name))
+        JSObjectSetProperty(self.js_ctx, self.js_val, js_name, js_val, 0)
+        return space.w_None
 
 JSObject.typedef = TypeDef("JSObject",
-        __getattribute__ = interp2app(JSObject.descr_getattribute,
-                                      unwrap_spec=['self', ObjSpace, str]),
-
+        __getattribute__ = interp2app(JSObject.descr_get,
+                                      unwrap_spec=['self', ObjSpace, W_Root]),
+        __getitem__ = interp2app(JSObject.descr_get,
+                                 unwrap_spec=['self', ObjSpace, W_Root]),
+        __setitem__ = interp2app(JSObject.descr_set,
+                              unwrap_spec=['self', ObjSpace, W_Root, W_Root]),
+        __setattr__ = interp2app(JSObject.descr_set,
+                              unwrap_spec=['self', ObjSpace, W_Root, W_Root]),
 )
--- source/client/webkit_bridge/test/test_applevel.py
+++ source/client/webkit_bridge/test/test_applevel.py
@@ -11,5 +11,14 @@ class AppTestBindings(object):
         ctx = JSGlobalContextCreate()
         cls.w_js_obj = space.wrap(JSObject(ctx, empty_object(ctx)))
     
-    def test_getsetattr_none(self):
+    def test_getattr_none(self):
         assert self.js_obj.x == None
+
+    def test_getsetattr_obj(self):
+        self.js_obj['x'] = 3
+        assert isinstance(self.js_obj.x, float)
+        assert self.js_obj.x == 3
+        self.js_obj.y = 3
+        assert isinstance(self.js_obj['y'], float)
+        assert self.js_obj['y'] == 3
+        
