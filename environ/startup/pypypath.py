d50c6933651a6ac3eceb01a5b7c654e1840e7b72
fijal <fijal@zygfryd.(none)>
2009-07-09 14:00:32 -0600

    Add a way of building shared pypy. It should not require libJavaScriptCore.so for building, only for testing.

--- environ/startup/pypypath.py
+++ environ/startup/pypypath.py
@@ -3,4 +3,7 @@ import sys, os
 
 pypy_path = os.path.join(os.path.split(__file__)[0], '..', '..',
                          'third_party', 'generic', 'pypy')
+webkit_bridge_path = os.path.join(os.path.split(__file)[0], '..', '..',
+                                  'source', 'client')
 sys.path.insert(0, pypy_path)
+sys.path.insert(0, webkit_bridge_path)
