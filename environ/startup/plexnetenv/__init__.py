e7970dbeffd642e4182ccc493fbe4fde2c07359d
tav <tav@espians.com>
2009-12-01 13:07:32 +0000

    Removing our previous gclient 0.1 version and updating plexnetenv python package to include DEPOT_TOOLS in sys.path.

--- environ/startup/plexnetenv/__init__.py
+++ environ/startup/plexnetenv/__init__.py
@@ -13,9 +13,10 @@ PLEXNET_LOCAL = join_path(PLEXNET_ROOT, 'environ', 'local')
 PLEXNET_SOURCE = join_path(PLEXNET_ROOT, 'source')
 THIRD_PARTY = join_path(PLEXNET_ROOT, 'third_party')
 PYTHON_SITE_PACKAGES = join_path(PLEXNET_ROOT, 'third_party', 'python')
+DEPOT_TOOLS = join_path(PLEXNET_ROOT, 'third_party', 'generic', 'depot_tools')
 
-if PYTHON_SITE_PACKAGES not in sys.path:
-    sys.path.insert(0, PYTHON_SITE_PACKAGES)
-
-if PLEXNET_SOURCE not in sys.path:
-    sys.path.insert(0, PLEXNET_SOURCE)
+for path in [
+    DEPOT_TOOLS, PYTHON_SITE_PACKAGES, PLEXNET_SOURCE
+    ]:
+    if path not in sys.path:
+        sys.path.insert(0, path)
